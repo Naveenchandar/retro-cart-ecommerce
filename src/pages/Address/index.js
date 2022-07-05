221c756d6818bdf36322430b9368fb472ae9cee1
Naveenchandar <naveenchandece@gmail.com>
2022-07-05 23:33:02 +0530

    delivery address flow added

--- src/pages/Address/index.js
+++ src/pages/Address/index.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from 'react'
 import toast from 'react-hot-toast';
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
 import { useGetLocalStorage, useSetLocalStorage } from '../../hooks/useLocalStorage';
 import { fetchItemById, updateItemById } from '../../utils';
 import { AddressForm } from './form';
 import './index.css';
+import { useNavigate } from 'react-router-dom';
+import { useWindowSize } from '../../hooks/useWindowSize';
 
 export const Address = () => {
     const [showAddressForm, setShowAddressForm] = useState(false);
     const [editAddressForm, setEditAddressForm] = useState('');
+    const [deliverySelected, isDeliverySelected] = useState(false);
     const setLocalStorage = useSetLocalStorage('retro-cart-address');
     const getLocalStorage = useGetLocalStorage('retro-cart-address');
     const [addressList, setAddressList] = useState(getLocalStorage('retro-cart-address') || []);
+    const navigate = useNavigate();
+    const { width, height } = useWindowSize()
 
     const addAddress = (address) => {
         if (editAddressForm) {
@@ -57,47 +62,62 @@ export const Address = () => {
         ))
     )
 
+    const deliveryClick = () => {
+        isDeliverySelected(true);
+        // navigate('/products', { replace: true })
+    }
+
+    if (!deliverySelected) {
+        return (
+            <section className='flex align_center flex_dcolumn address_section'>
+                <h4 className='text_center'>Manage Address</h4>
+                <button
+                    className='btn btn_primary font_bold text_uppercase text_center'
+                    onClick={() => { setShowAddressForm(!showAddressForm); setEditAddressForm(''); }}
+                >
+                    Add new address
+                </button>
+                {showAddressForm && <AddressForm addAddress={addAddress} edit={editAddressForm} />}
+                {addressList?.length ? (
+                    <section className='flex border address_list'>
+                        {addressList?.map(({ id, name, phoneNum, country, state, landmark, fullAddress, pincode }) => {
+                            return (
+                                <section key={id} className='border address_list_item'>
+                                    <h4 className='flex'>
+                                        <span title={name}>Name: {name}</span>
+                                        <div className='address_icons'>
+                                            <AiOutlineEdit
+                                                className='pointer'
+                                                title='Edit Address'
+                                                onClick={() => editAddress(id)}
+                                            /> &nbsp;
+                                            <AiOutlineDelete className='pointer' title='Delete Address' />
+                                        </div>
+                                    </h4>
+                                    {renderAddressInfo(
+                                        ['Phone number', 'Address', 'Landmark', 'Pincode', 'State', 'Country'],
+                                        [phoneNum, fullAddress, landmark, pincode, state, country]
+                                    )}
+                                    <button
+                                        className='btn btn_primary font_bold text_uppercase'
+                                        onClick={deliveryClick}
+                                    >Delivery here</button>
+                                </section>
+                            )
+                        })}
+                    </section>
+                ) : <p className='text_center'>
+                    {showAddressForm ? '' : 'No Address to add. Please add new address to proceed'}</p>
+                }
+            </section>
+        )
+    }
     return (
         <section className='flex align_center flex_dcolumn address_section'>
-            <h4 className='text_center'>Manage Address</h4>
-            <button
-                className='btn btn_primary font_bold text_uppercase text_center'
-                onClick={() => { setShowAddressForm(!showAddressForm); setEditAddressForm(''); }}
-            >
-                Add new address
-            </button>
-            {showAddressForm && <AddressForm addAddress={addAddress} edit={editAddressForm} />}
-            {addressList?.length ? (
-                <section className='flex border address_list'>
-                    {addressList?.map(({ id, name, phoneNum, country, state, landmark, fullAddress, pincode }) => {
-                        return (
-                            <section key={id} className='border address_list_item'>
-                                <h4 className='flex'>
-                                    <span title={name}>Name: {name}</span>
-                                    <div className='address_icons'>
-                                        <AiOutlineEdit
-                                            className='pointer'
-                                            title='Edit Address'
-                                            onClick={() => editAddress(id)}
-                                        /> &nbsp;
-                                        <AiOutlineDelete className='pointer' title='Delete Address' />
-                                    </div>
-                                </h4>
-                                {renderAddressInfo(
-                                    ['Phone number', 'Address', 'Landmark', 'Pincode', 'State', 'Country'],
-                                    [phoneNum, fullAddress, landmark, pincode, state, country]
-                                )}
-                                <button className='btn btn_primary font_bold text_uppercase'>Delivery here</button>
-                            </section>
-                        )
-                    })}
-                </section>
-            ) : <p className='text_center'>
-                {showAddressForm ? '' : 'No Address to add. Please add new address to proceed'}</p>
-            }
+            <h4 className='text_center'>Congrats, Your order got successfully placed</h4>
             <Confetti
-                width={600}
-                height={600}
+                width={width}
+                height={height}
             />
         </section>
     )
