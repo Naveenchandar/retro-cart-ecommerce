8cce579ac1fe0f814241782be611523146a99d26
Naveenchandar <naveen.chandar@tranetechnologies.com>
2022-07-27 19:14:10 +0530

    react router converted with hooks changes

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,52 +1,44 @@
-import { Routes, Route, Link } from "react-router-dom";
-import { LoginPage } from 'pages';
-import { SignUp } from 'components/auth/signup';
+import { Link, useRoutes } from "react-router-dom";
 import Mockman from "mockman-js";
+import { LoginPage } from 'pages';
+import { SignUp } from 'components';
 import {
     Home, Products, WishList, CartManagement, Address
-} from '../../pages';
+} from 'pages';
 import { PrivateRoutes } from './PrivateRoutes';
 import './index.css';
 
-export const NavRoutes = () => {
+const NoRouteFound = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/product/:productName" element={<Products />} />
-            <Route path="/wishlist" element={
-                <PrivateRoutes>
-                    <WishList />
-                </PrivateRoutes>
-            }
-            />
-            <Route path="/cart" element={
-                <PrivateRoutes>
-                    <CartManagement />
-                </PrivateRoutes>
-            }
-            />
-            <Route path="/address" element={
-                <PrivateRoutes>
-                    <Address />
-                </PrivateRoutes>
-            }
-            />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/mock" element={<Mockman />} />
-            <Route
-                path="*"
-                element={
-                    <div className='not_found flex flex_dcolumn justify_center align_center'>
-                        <p className="error404">404 | Page Not Found!</p>
-                        <p className="error404-msg">
-                            Oops!! Looks like you have entered a wrong URL
-                        </p>
-                        <Link to='/' className="btn btn_primary">Back to home</Link>
-                    </div>
-                }
-            />
-        </Routes>
+        <div className='not_found flex flex_dcolumn justify_center align_center'>
+            <p className="error404">404 | Page Not Found!</p>
+            <p className="error404-msg">
+                Oops!! Looks like you have entered a wrong URL
+            </p>
+            <Link to='/' className="btn btn_primary">Back to home</Link>
+        </div>
     )
-}
+};
+
+const Router = () => {
+    const routes = [
+        {
+            children: [
+                { path: "/", element: <Home /> },
+                { path: "products", element: <Products /> },
+                { path: "product/:productName", element: <Products /> },
+                { path: "wishlist", element: <PrivateRoutes><WishList /></PrivateRoutes> },
+                { path: "cart", element: <PrivateRoutes><CartManagement /></PrivateRoutes> },
+                { path: "address", element: <PrivateRoutes><Address /></PrivateRoutes> },
+                { path: "login", element: <LoginPage /> },
+                { path: "signup", element: <SignUp /> },
+                { path: "mock", element: <Mockman /> },
+                { path: "*", element: <NoRouteFound /> }
+            ]
+        }
+    ];
+    let element = useRoutes(routes);
+    return element;
+};
+
+export const NavRoutes = () => <Router />
